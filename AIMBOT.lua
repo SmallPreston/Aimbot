ddd0298f1ba0c13d04da0f551611e40e700f3885
SmallPreston <112457080+SmallPreston@users.noreply.github.com>
2022-10-29 12:59:37 -0300

    Update AIMBOT.lua

--- AIMBOT.lua
+++ AIMBOT.lua
@@ -12,7 +12,7 @@ _G.AimPart = "Head" -- Where the aimbot script would lock at.
 _G.Sensitivity = 0 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
 
 _G.CircleSides = 64 -- How many sides the FOV circle would have.
-_G.CircleColor = Color3.fromRGB(255, 255, 255) -- (RGB) Color that the FOV circle would appear as.
+_G.CircleColor = Color3.fromRGB(255, 0, 0) -- (RGB) Color that the FOV circle would appear as.
 _G.CircleTransparency = 0.7 -- Transparency of the circle.
 _G.CircleRadius = 80 -- The radius of the circle / FOV.
 _G.CircleFilled = false -- Determines whether or not the circle is filled.
